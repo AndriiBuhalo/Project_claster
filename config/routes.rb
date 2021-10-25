ae76853e1af793f4d4df00ffff62b9bc197eda4a
AndriiBuhalo <andriubuhal@gmail.com>
2021-10-25 13:39:09 +0300

    add validation and some test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   get 'secrets/index'
   devise_for :users
   root to: 'home#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-FactoryBot.create_list(:user,2)
+FactoryBot.create_list(:user,4)
+User.find_or_create(name:'admin', email: 'admin@admin', admin: true )
