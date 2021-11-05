171c74598da626b717fbf437d626bceb7230f5ca
AndriiBuhalo <andriubuhal@gmail.com>
2021-11-05 16:31:49 +0200

    Add aboute contriller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'about/index'
   resources :microposts do
     member do
       post "vote", to: "microposts#vote"
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
     resources :comments 
       
   end
-  
   get 'secrets/index'
   devise_for :users
   root to: 'home#index'
